419b83d984147f1fc312394ef0b66296171efc52
Tristan Paul <tpaul016@uottawa.ca>
2020-06-25 15:42:21 -0400

    process dpa to json file

---
+++ src/data/make_dataset.py
@@ -0,0 +1,26 @@
+import re
+import json
+
+contract = open('../../data/contracts/DPA Data Processing Addendum - 2019 - Final.txt', 'r').read()
+sentence_list = re.split(r'\n+ ?\t*', contract)
+
+sentences = []
+for i, sentence in enumerate(sentence_list):
+    if sentence == '':
+        continue
+    sentences.append({
+        'id': i,
+        'text': sentence,
+        'type': 'regular',
+        'symboleo_elements': {
+            'trigger': None,
+            'debtor': None,
+            'creditor': None,
+            'antecedent': None,
+            'consequent': None,
+            'asset': None
+        }
+    })
+
+open('../../data/processed/dpa_labeled_draft.json', 'w').write(json.dumps(sentences))
+
