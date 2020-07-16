95fc0047810432eaeb88e7cedf4931f78fcdfedb
Tristan Paul <tpaul016@uottawa.ca>
2020-07-16 11:33:20 -0400

    changed implementation to work with any amount of sub_clauses

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,128 +3,65 @@ import json
 
 STATEMENT_RE = re.compile(r'\n+ ?\t*')
 SENTENCE_RE = re.compile(r'(?<=(?<![0-9])[.!?])\s+|(?<=[.!?])?\s*\n+')
-CLAUSE_RE = re.compile(r'\d+\.\s+.*')
-SUB_CLAUSE_RE = re.compile(r'\d+\.\d+\s+.*')
-SUB_SUB_CLAUSE_RE = re.compile(r'\d+\.\d+\.\d+\s+.*')
+ALL_CLAUSE_RE = re.compile(r'\d+\.(\d+\.?)*\s+.*')
 
 contract_text = open('../../data/contracts/DPA Data Processing Addendum - 2019 - Final.txt', 'r').read()
 
-# extract statements
 statement_list = STATEMENT_RE.split(contract_text)
 
 contract = {}
 clauses = []
-clause = {}
-sub_clause = {}
-sub_sub_clause = {}
-idx = 0
+clause_stack = []
 sentence_id = 1
 
-while idx < len(statement_list):
-    statement = statement_list[idx]
-    if CLAUSE_RE.match(statement):
-        if sub_sub_clause:
-            sub_clause['sub_clauses'].append(sub_sub_clause)
-            sub_sub_clause = {}
-        if sub_clause:
-            clause['sub_clauses'].append(sub_clause)
-            sub_clause = {}
-        if clause:
-            clauses.append(clause)
+for statement in statement_list:
+    if ALL_CLAUSE_RE.match(statement):
+        title_number, title_text = statement.split('\t')
+        clause_level = len(re.findall(r'\d+', title_number))
 
-        title_list = statement.split('\t')
+        # remove all clauses from the stack that are the same or lower level
+        while len(clause_stack) > 0 and len(re.findall(r'\d+', clause_stack[-1]['title_number'])) >= clause_level:
+            # top level clauses are added to clauses list
+            if len(re.findall(r'\d+', clause_stack[-1]['title_number'])) == 1:
+                clauses.append(clause_stack[-1])
+            clause_stack.pop()
+
         clause = {
-            'title_number': title_list[0],
-            'title_text': title_list[1].strip(),
+            'title_number': title_number,
+            'title_text': title_text.strip(),
             'sentences': [],
             'sub_clauses': []
         }
-    elif SUB_CLAUSE_RE.match(statement):
-        if sub_sub_clause:
-            sub_clause['sub_clauses'].append(sub_sub_clause)
-            sub_sub_clause = {}
-        if sub_clause:
-            clause['sub_clauses'].append(sub_clause)
 
-        title_list = statement.split('\t')
-        sub_clause = {
-            'title_number': title_list[0],
-            'title_text': title_list[1].strip(),
-            'sentences': [],
-            'sub_clauses': []
-        }
-    elif SUB_SUB_CLAUSE_RE.match(statement):
-        if sub_sub_clause:
-            sub_clause['sub_clauses'].append(sub_sub_clause)
+        if len(clause_stack) > 0:
+            clause_stack[-1]['sub_clauses'].append(clause)
 
-        title_list = statement.split('\t')
-        sub_sub_clause = {
-            'title_number': title_list[0],
-            'title_text': title_list[1].strip(),
-            'sentences': [],
-            'sub_clauses': []
-        }
-    else:
+        clause_stack.append(clause)
+
+    elif len(clause_stack) > 0:
         for sentence in SENTENCE_RE.split(statement):
             if not sentence:
                 continue
-            elif sub_sub_clause:
-                sub_sub_clause['sentences'].append({
-                    'id': sentence_id,
-                    'text': sentence,
-                    'type': 'regular',
-                    'modality': None,
-                    'symboleo_elements': {
-                        'trigger': None,
-                        'debtor': None,
-                        'creditor': None,
-                        'antecedent': None,
-                        'consequent': None,
-                        'asset': None
-                    }
-                })
-            elif sub_clause:
-                sub_clause['sentences'].append({
-                    'id': sentence_id,
-                    'text': sentence,
-                    'type': 'regular',
-                    'modality': None,
-                    'symboleo_elements': {
-                        'trigger': None,
-                        'debtor': None,
-                        'creditor': None,
-                        'antecedent': None,
-                        'consequent': None,
-                        'asset': None
-                    }
-                })
-            # ignore statements outside of clauses for now
-            elif not clause:
-                continue
-            else:
-                clause['sentences'].append({
-                    'id': sentence_id,
-                    'text': sentence,
-                    'type': 'regular',
-                    'modality': None,
-                    'symboleo_elements': {
-                        'trigger': None,
-                        'debtor': None,
-                        'creditor': None,
-                        'antecedent': None,
-                        'consequent': None,
-                        'asset': None
-                    }
-                })
-            sentence_id += 1
-    idx += 1
 
-if sub_sub_clause:
-    sub_clause['sub_clauses'].append(sub_sub_clause)
-if sub_clause:
-    clause['sub_clauses'].append(sub_clause)
-if clause:
-    clauses.append(clause)
+            clause_stack[-1]['sentences'].append({
+                'id': sentence_id,
+                'text': sentence,
+                'type': 'regular',
+                'modality': None,
+                'symboleo_elements': {
+                    'trigger': None,
+                    'debtor': None,
+                    'creditor': None,
+                    'antecedent': None,
+                    'consequent': None,
+                    'asset': None
+                }
+            })
+
+            sentence_id += 1
+
+if len(clause_stack) > 0:
+    clauses.append(clause_stack[0])
 
 contract['clauses'] = clauses
 open('../../data/processed/dpa_labeled_draft.json', 'w').write(json.dumps(contract))
